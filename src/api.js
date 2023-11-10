3029774f04cfcacfe6f57f64bb3716bae339c1b1
Cashton Holbert <cashtoncoleholbert@gmail.com>
2023-11-09 16:39:43 -0800

    https for secure request

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 const getWeather = async (location) => {
   try {
-    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=2678e6bb9fa24eb594e183637230611&q=${location}&days=3`);
+    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=2678e6bb9fa24eb594e183637230611&q=${location}&days=3`);
     const data = await response.json();
     return data;
   } catch (response) {
